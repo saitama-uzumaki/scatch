19c7cf568e0baa252b5437e382e6f02914e20b0f
unknown <rawataryan293@gmail.com>
2025-06-05 18:10:35 +0530

    created owner post

--- models/owner-model.js
+++ models/owner-model.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
 
 const ownerSchema = mongoose.Schema({
-    fullname : String,
+    fullname :{
+        type : String,
+        minLength :3,
+        trim :true,
+    },
     email :    String,
     password : String,
     
-    isadmin : Boolean,
+    
     products :{
         type:Array,
         default:[],
     },
-    contact : Number,
+    
     picture:String,
     gstin:String,
 
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+const ownerModel = require("../models/owner-model");
+
+
+
 router.get("/", function (req,res){
     res.send("hey its working ");
+});
+
+router.post("/create", async function (req,res){
+   let owners = await ownerModel.find();
+   if (owners.length > 0) {
+    return res
+    .send(503)
+    .send("you dont have permission to create a new owner");
+}
+
+    let {fullname , email , password} = req.body;
+let createdOwner = await ownerModel.create({
+    fullname ,
+    email ,
+    password ,   
+});
+res.status(201).send(createdOwner);
 });
 
 module.exports = router;
