5a8a4768874896f0fd2e4c418ed28f0cbeb67d8a
unknown <rawataryan293@gmail.com>
2025-06-05 12:52:45 +0530

    connected to mongoodb

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+require("dotenv").config();
 
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
+const connectDB = require("./config/mongoose-connection");
+connectDB(); // Important!
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,"public")));
-app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req,res) => {
-    res.send("hey");
-})
+app.set("view engine", "ejs");
 
-app.listen(3000);
+app.get("/", (req, res) => {
+  res.send("hey");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- models/owner-model.js
+++ models/owner-model.js
@@ -1,21 +1,19 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
+const ownerSchema = mongoose.Schema({
     fullname : String,
     email :    String,
     password : String,
-    cart :   {
-        type:Array,
-        default:[],
-    },
+    
     isadmin : Boolean,
-    orders :{
+    products :{
         type:Array,
         default:[],
     },
     contact : Number,
     picture:String,
+    gstin:String,
 
 });
 
-module.exports = mongoose.model("user",userSchema);
+module.exports = mongoose.model("owner",ownerSchema);
