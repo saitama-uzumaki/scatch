870fd9b1e3bb154fca972a0d900db83ab90bbd93
unknown <rawataryan293@gmail.com>
2025-06-25 11:15:30 +0530

    fix cart rendering , add null checks , discount logic ,total computation

--- models/user-model.js
+++ models/user-model.js
@@ -4,10 +4,18 @@ const userSchema = mongoose.Schema({
     fullname : String,
     email :    String,
     password : String,
-    cart :   {
-        type:Array,
-        default:[],
+    cart: [
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "product",
     },
+    quantity: {
+      type: Number,
+      default: 1,
+    },
+  }
+],
     
     orders :{
         type:Array,
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const isLoggedin =require("../middlewares/isLoggedin");
 const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
 
 
 router.get("/", function (req, res) {
     let error = req.flash("error");
-    res.render("index.ejs", { error: "" }); // 👈 pass a default error value
+    res.render("index.ejs", { error , isLoggedin :false }); // 👈 pass a default error value
 });
 
 // router.get("/shop",isLoggedin,async function (req,res) {
@@ -20,8 +21,66 @@ router.get("/", function (req, res) {
 
 router.get("/shop",isLoggedin,async function (req,res){
     let products = await productModel.find();
-    res.render("shop",{products});
+   
+    let success = req.flash("success");
+    res.render("shop",{products , success});
 });
+
+// router.get("/cart",isLoggedin,async function (req,res){
+//     let user = await userModel.findOne({email : req.user.email}).populate("cart.product");
+//     res.render("cart",{user});
+// });
+
+// router.get("/addtocart/:id", isLoggedin, async function (req, res) {
+//     let user = await userModel.findOne({ email: req.user.email });
+
+//     // Ensure cart is always initialized
+//     if (!user.cart) user.cart = [];
+
+//     // Safe findIndex with null checks
+//     let productIndex = user.cart.findIndex(item =>
+//         item && item.product && item.product.toString() === req.params.id
+//     );
+
+//     if (productIndex > -1) {
+//         user.cart[productIndex].quantity += 1;
+//     } else {
+//         user.cart.push({ product: req.params.id, quantity: 1 });
+//     }
+
+//     await user.save();
+//     req.flash("success", "added to cart");
+//     res.redirect("/shop");
+// }); 
+
+// ✅ Updated /addtocart route
+router.get("/addtocart/:id", isLoggedin, async function (req, res) {
+    let user = await userModel.findOne({ email: req.user.email });
+
+    if (!user.cart) user.cart = [];
+
+    let productIndex = user.cart.findIndex(item =>
+        item && item.product && item.product.toString() === req.params.id
+    );
+
+    if (productIndex > -1) {
+        user.cart[productIndex].quantity += 1;
+    } else {
+        user.cart.push({ product: req.params.id, quantity: 1 });
+    }
+
+    await user.save();
+    req.flash("success", "Added to cart");
+    res.redirect("/shop");
+});
+
+
+// ✅ Updated /cart route with cart.product population
+router.get("/cart", isLoggedin, async function (req, res) {
+    let user = await userModel.findOne({ email: req.user.email }).populate("cart.product");
+    res.render("cart", { user });
+});
+
 
 router.get("/logout",isLoggedin, function (req, res) {
    res.render("shop"); // 👈 pass a default error value
